5d3d196b3ea39257a94a861c85b2e8b566d0d788
Neider Vélez <velezzambranoneider@gmail.com>
2026-06-11 14:23:25 -0500

    feat: add values.properties
    feat: add some @Value to PathVariableController

---
+++ 01-helloworld/src/main/java/imnider/learning/springboot/helloworld/ValuesConfig.java
@@ -0,0 +1,13 @@
+package imnider.learning.springboot.helloworld;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+    @PropertySource(value="classpath:values.properties", encoding="UTF-8")
+})
+public class ValuesConfig {
+
+}
--- 01-helloworld/src/main/java/imnider/learning/springboot/helloworld/controllers/PathVariableController.java
+++ 01-helloworld/src/main/java/imnider/learning/springboot/helloworld/controllers/PathVariableController.java
@@ -1,15 +1,44 @@
 package imnider.learning.springboot.helloworld.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import imnider.learning.springboot.helloworld.models.dto.ParamDto;
+import imnider.learning.springboot.helloworld.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api/path")
 public class PathVariableController {
+    @Value("${config.code}")
+    private String configCode;
+
+    @Value("${config.list}")
+    private List<String> languagesListDefault;
+
+    @Value("#{ '${config.list}'.toUpperCase().split(',') }")
+    private List<String> languagesList;
+
+    @Value("#{ '${config.list}' }")
+    private String languagesListString;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String, String> valuesMap;
+
+    @Value("#{${config.valuesMap}.name}")
+    private String userName;
+
+    @Value("#{${config.valuesMap}.email}")
+    private String userEmail;
+
     @GetMapping("/foo/{code}")
     public ParamDto foo(@PathVariable Integer code) {
         return new ParamDto(code.toString(), code);
@@ -18,5 +47,24 @@ public class PathVariableController {
     @GetMapping("/bar/{message}/{code}")
     public ParamDto bar(@PathVariable String message, @PathVariable Integer code) {
         return new ParamDto(message, code);
+    }
+
+    @PostMapping("/create")
+    public UserDto createUser(@RequestBody UserDto userDto) {
+        return userDto;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String configMessage) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("code", configCode);
+        response.put("message", configMessage);
+        response.put("languagesDefault", languagesListDefault);
+        response.put("languagesList", languagesList);
+        response.put("languagesListString", languagesListString);
+        response.put("valuesMap", valuesMap);
+        response.put("userName", userName);
+        response.put("userEmail", userEmail);
+        return response;
     }
 }
