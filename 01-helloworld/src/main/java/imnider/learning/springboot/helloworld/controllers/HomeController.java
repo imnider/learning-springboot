571754046a5263f66d16fb3f24b8dba283161e9d
Neider Vélez <velezzambranoneider@gmail.com>
2026-06-11 14:57:11 -0500

    feat: add redirect to /home, /, and empty

---
+++ 01-helloworld/src/main/java/imnider/learning/springboot/helloworld/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package imnider.learning.springboot.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home", ""})
+    public String home() {
+        return "redirect:/profile";
+    }
+
+}
