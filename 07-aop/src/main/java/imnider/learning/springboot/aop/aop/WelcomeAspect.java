5ad30fac612157e6c66578736417c7560f35e41b
Neider Vélez <velezzambranoneider@gmail.com>
2026-06-16 09:49:23 -0500

    feat: add after advice in welcome aspect

--- 07-aop/src/main/java/imnider/learning/springboot/aop/aop/WelcomeAspect.java
+++ 07-aop/src/main/java/imnider/learning/springboot/aop/aop/WelcomeAspect.java
@@ -3,6 +3,7 @@ package imnider.learning.springboot.aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -19,7 +20,13 @@ public class WelcomeAspect {
     public void loggerBefore(JoinPoint joinPoint){
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
-        logger.info("Method: ".concat(method).concat(". Args: ").concat(args));
+        logger.info("@Before. Method: ".concat(method).concat(". Args: ").concat(args));
     }
 
+    @After("execution(String imnider.learning.springboot.aop.services.IWelcomeService.sayHello(..))")
+    public void loggerAfter(JoinPoint joinPoint){
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("@After. Method: ".concat(method).concat(". Args: ").concat(args));
+    }
 }
