5467eaf66e35d2c09a55c121dcd4fb8b9467aea2
Neider Vélez <velezzambranoneider@gmail.com>
2026-06-18 12:14:38 -0500

    feat: add address entity to test one-to-many relationship

--- 09-relationship/src/main/java/imnider/learning/springboot/relationship/entities/Client.java
+++ 09-relationship/src/main/java/imnider/learning/springboot/relationship/entities/Client.java
@@ -1,9 +1,14 @@
 package imnider.learning.springboot.relationship.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,11 +21,16 @@ public class Client {
 
     private String name;
     private String lastname;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses;
     
     public Client() {
+        addresses = new ArrayList<>();
     }
 
     public Client(String name, String lastname) {
+        this();
         this.name = name;
         this.lastname = lastname;
     }
@@ -43,10 +53,16 @@ public class Client {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
 
     @Override
     public String toString() {
-        return "{id=" + id + ", name=" + name + ", lastname=" + lastname + "}";
+        return "{id=" + id + ", name=" + name + ", lastname=" + lastname + ", addresses=" + addresses + "}";
     }
 
 }
